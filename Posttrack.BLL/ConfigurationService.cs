b59cccc7f3e71bba66e75b9c64816b64102f8300
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2018-10-23 20:42:12 +0300

    fix searching regex

--- Posttrack.BLL/ConfigurationService.cs
+++ Posttrack.BLL/ConfigurationService.cs
@@ -13,7 +13,7 @@ namespace Posttrack.BLL
 
         public int InactivityPeriodMonths => 2;
 
-        public string HistoryRegex => @"(\d{4}-\d{2}-\d{2}|\d{2}.\d{2}.\d{4}).+?(?>Label1"" >)(.+?(?=</span>))(.+?(?>size=""4"">)([^<]+)</font></td>\r\n\t\t\t\t</tr>)?";
+        public string HistoryRegex => @"(\d{4}-\d{2}-\d{2}|\d{2}.\d{2}.\d{4}).+?(?>Label1"">)(.+?(?=</span>))(.+?(?>size=""4"">)([^<]+)</font></td>\r\n\t\t\t\t</tr>)?";
 
         public string HttpSearchUrl => "http://declaration.belpost.by/searchRu.aspx";
     }
